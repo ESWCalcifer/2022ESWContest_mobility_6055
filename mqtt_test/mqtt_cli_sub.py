cbf7a46cad52032238b45eca36988541d61c86be
Dylee-y <dlekdls0121@gmail.com>
2022-10-06 11:20:58 +0900

    mqtt code of master pi

--- mqtt_test/mqtt_cli_sub.py
+++ mqtt_test/mqtt_cli_sub.py
@@ -5,7 +5,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("/home/pim/esw/test_mqtt")
+    client.subscribe("/home/pim/esw/test_mqtt/gesture")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
