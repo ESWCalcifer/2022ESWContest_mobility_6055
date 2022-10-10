ce41968e92852b5fdeb797122d3dd82f6387951e
easyblank <clapd10@khu.ac.kr>
2022-10-11 04:18:01 +0900

    wip

--- hand_pose_classification/scene.py
+++ hand_pose_classification/scene.py
@@ -6,25 +6,25 @@ from datetime import datetime
 import paho.mqtt.client as mqtt
 import imageio
 class Scene():
+    client = mqtt.Client()
     def __init__(self):
         self.image_lst = []
-        self.cap_usb= cv2.VideoCapture(-1)
+        self.cap_usb= cv2.VideoCapture(0)
         self.message = str
-        self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.client.connect_async("192.168.0.200",1883, 60)
         self.exist = False
         self.last_gif = str
-        self.client.loop_start()
         # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe("/home/pim/esw/test_mqtt/gesture")
+        self.client.subscribe("/home/pim/esw/test_mqtt/gesture")
 
         # The callback for when a PUBLISH message is received from the server.
+    def start(self):        
+        self.client.connect("192.168.0.200",1883, 60)
     def on_message(self, client, userdata, msg):
         self.message = str(msg.payload.decode("utf-8"))
     def is_exist(self):
@@ -35,16 +35,17 @@ class Scene():
         else:
             return None
     def start_loop(self):
-        while True:
-            if self.cap_usb.isOpened():
-                if self.message == "startsig":
-                    success_usb, frame_usb = self.cap_usb.read()
-                    image_lst.append(frame_usb)
-            if self.message == "stopsig" and len(image_lst) is not 0:
+        self.client.loop_start()
+        if self.cap_usb.isOpened():
+            if self.message == "startsig":
+                success_usb, frame_usb = self.cap_usb.read()
+                self.image_lst.append(frame_usb)
+            if self.message == "stopsig" and len(self.image_lst) > 0:
                 curr_time = datetime.now().strftime("%H%M%S")
-                imageio.mimsave(f'./video_{curr_time}.gif', image_lst, fps = 10)
+                imageio.mimsave(f'./video_{curr_time}.gif', self.image_lst, fps = 10)
                 self.last_gif = f"./video_{curr_time}.gif"
                 cv2.imwrite(f"./frame_{curr_time}.jpg", frame_usb)
-                image_lst = []
+                self.image_lst = []
                 self.exist = True
-
+cscene = Scene()
+cscene.start()
--- hand_pose_classification/stream_video.py
+++ hand_pose_classification/stream_video.py
@@ -3,20 +3,21 @@ from flask import Flask, send_from_directory
 from flask_executor import Executor
 from flask_cors import CORS
 from bson.json_util import dumps
+from pathlib import Path
 app = Flask(__name__)
 CORS(app)
-scene = Scene()
+c_scene = Scene()
 executor = Executor(app)
 @app.route('/job')
 def index():
-    executor.submit(Scene.start_loop())
-    return "submitted"
+    while True:
+        executor.submit(c_scene.start_loop())
 @app.route('/scene')
 def video_feed():
-    return send_from_directory('gif', Scene.get_last_gif())
+    return send_from_directory('gif',Path(c_scene.get_last_gif()))
 
 @app.route("/get_first_camera", methods = ['GET'])
 def get_first_camera():
-    return dumps({'detected' : "True" if Scene.is_exist() else "False"})
+    return dumps({'detected' : "True" if c_scene.is_exist() else "False"})
 
 app.run(host='0.0.0.0', port='5000', debug=False)
